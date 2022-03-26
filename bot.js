01d381cad40fbb869af235ef5a30375cb7d9fe49
abduljabbar1a <102430847+abduljabbar1a@users.noreply.github.com>
2022-03-26 23:10:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,23 +3,3 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '5266816142:AAHG9RC8tQ4j57Sy0s1ae94hAYI1nIoWxSQ';
 var bot = new TelegramBot(token, {polling: true});
 bot.on("polling_error", (err) => console.log(err));
-
-bot.on("message",function(msg){
-      var monn = msg.text;
-  if(msg.text == '/start'){
-  rep = 'يسعدنا إنضمامك الى مجموعة حل تمارين الهندسة';
-  tito='اضغط للاشتراك في المجموعة ';
-  
- bot.sendMessage(msg.chat.id,'hello', {
-  reply_markup: {
-      inline_keyboard: [
-          [{
-              text: 'pathh' ,
-              url: 'https'
-          }]
-                       ]
-                 }
-                                  });
-                               
-});
-};
